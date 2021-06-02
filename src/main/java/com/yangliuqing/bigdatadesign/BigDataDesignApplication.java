360b7f20a2e194f71587ee38fdb28889fb37d4e2
0102-yang <1657375983@qq.com>
2021-06-02 10:10:35 +0800

    Initial commit

---
+++ src/main/java/com/yangliuqing/bigdatadesign/BigDataDesignApplication.java
@@ -0,0 +1,13 @@
+package com.yangliuqing.bigdatadesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigDataDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yangliuqing/bigdatadesign/BigDataDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangliuqing.bigdatadesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
